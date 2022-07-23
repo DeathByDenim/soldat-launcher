6ba13f50603f4a4dca1ddf89f6b58dc932430903
DeathByDenim <jarno@jarno.ca>
2022-07-23 14:33:29 -0400

    Save launcher settings in appdir

--- src/api/launcher/data.ts
+++ src/api/launcher/data.ts
@@ -1,19 +1,27 @@
 import fs from "fs";
 import path from "path";
 import { isProduction } from "src/environment";
+import { ipcRenderer } from "electron";
 
-const filePath = path.resolve(isProduction ? process.resourcesPath : "", "./launcher.json");
+const appDir = ipcRenderer.invoke('getAppDataPath');
+const filePath = appDir.then((dir: string) => {
+    return path.resolve(isProduction ? dir : "", "./launcher.json");
+});
 
 const loadData = function(): Promise<string> {
-    return fs.promises.readFile(filePath, { encoding: "utf8" })
-        .catch((error) => {
-            console.warn("An error occurred when loading launcher's data", error);
-            return null;
-        });
+    return filePath.then((path) => {
+        return fs.promises.readFile(path, { encoding: "utf8" })
+            .catch((error) => {
+                console.warn("An error occurred when loading launcher's data", error);
+                return null;
+            });
+    });
 }
 
 const saveData = function(fileContent: string): Promise<void> {
-    return fs.promises.writeFile(filePath, fileContent);
+    return filePath.then((path) => {
+        return fs.promises.writeFile(path, fileContent);
+    });
 }
 
 export { loadData, saveData };
